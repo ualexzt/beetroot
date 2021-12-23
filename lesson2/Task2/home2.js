9cfba299a744b844b165227b02661360bef65a47
AlexUa <ualexzt@gmail.com>
2021-12-23 12:03:44 +0200

    solution task2

---
+++ lesson2/Task2/home2.js
@@ -0,0 +1,38 @@
+/*
+ * Написати функцію, яка отримуватиме в аргумент
+ * масив votes та повертати масив унікальних значень
+ * відсортованих за кількістю їх входження до масиву votes
+ * Результат повинен вийти таким [react, vue, angular]
+ * */
+
+const votes = [
+  'angular',
+  'react',
+  'vue',
+  'react',
+  'angular',
+  'react',
+  'vue',
+  'react',
+  'vue',
+]
+
+function getVotes(list) {
+  // todo
+  const res = {}
+  list.forEach(item => {
+    if (item in res) {
+      res[item] += 1
+    } else res[item] = 0
+
+  })
+  return Object.entries(res)
+    .sort((a, b) => b[1] - a[1])
+    .reduce((acc, item) => {
+      console.log(item)
+      acc.push(item[0])
+      return acc
+    }, [])
+}
+
+console.log(getVotes(votes)) // [react, vue, angular]
