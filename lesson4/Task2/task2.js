6fe791b4e0ba4215e4bae6337187f40b6dc52fb2
AlexUa <ualexzt@gmail.com>
2022-01-04 17:11:35 +0200

    add homework to lesson4

--- lesson4/Task2/task2.js
+++ lesson4/Task2/task2.js
@@ -1,38 +1,42 @@
-const xhr = new XMLHttpRequest();
-xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
-xhr.onreadystatechange = handleResponse;
-xhr.send();
+// const xhr = new XMLHttpRequest()
+// xhr.open('GET', 'https://jsonplaceholder.typicode.com/users')
+// xhr.onreadystatechange = handleResponse
+// xhr.send()
 
-function handleResponse() {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    addUsersToPage(JSON.parse(xhr.response));
-  }
+// function handleResponse() {
+//   if (xhr.readyState === 4 && xhr.status === 200) {
+//     addUsersToPage(JSON.parse(xhr.response))
+//   }
+// }
+
+async function getJSON(url) {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  return await res.json()
 }
 
 function generateListItems(users) {
   return users
-    .map((u) => `<li class="list-group-item">${u.name}</li>`)
-    .join("");
+    .map((u) => `<li class="list-group-item">${u.name}</li>`).join('')
+
 }
 
 function generateUnorderedList(listItems) {
-  return `<ul class="list-group">${listItems}</ul>`;
+  return `<ul class="list-group">${listItems}</ul>`
 }
 
 function addUsersToPage(users) {
-  document.getElementById("users").innerHTML = generateUnorderedList(
-    generateListItems(users)
-  );
+  document.getElementById('users').innerHTML = users
 }
 
-/*  
+
+/*
 Провести refactoring кода, используя  Promise
 чтобы вызов функций был следующим
 */
 
-/*
-getJSON("https://jsonplaceholder.typicode.com/users")
-.then(generateListItems)
-.then(generateUnorderedList)
-.then(addUsersToPage);
-*/
+
+getJSON('https://jsonplaceholder.typicode.com/users')
+  .then(res => generateListItems(res))
+  .then(res => generateUnorderedList(res))
+  .then(res => addUsersToPage(res))
+
